5f5b54ce7e947ca2ee234dfc1935a489cef4d145
Micgael22 <micgael22@gmail.com>
2024-02-11 22:55:37 +0000

    baseconfig-incomplete

---
+++ src/test/java/base/BaseConfiguration.java
@@ -0,0 +1,41 @@
+package base;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Properties;
+
+public class BaseConfiguration extends BaseController {
+
+    public BaseConfiguration() throws MalformedURLException {
+    }
+    public static Properties prop;
+
+    public static Properties setCustomProperties(String sPath){
+        try {
+            prop = readPropertiesFile(sPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return prop;
+    }
+
+    public static Properties readPropertiesFile(String fileName) throws IOException {
+        FileInputStream fis = null;
+        Properties prop = null;
+        try {
+            fis = new FileInputStream(fileName);
+            prop = new Properties();
+            prop.load(fis);
+        }
+        catch (IOException ep) {
+            ep.printStackTrace();
+        }
+        finally {
+            assert fis != null;
+            fis.close();
+        }
+        return prop;
+    }
+
+}
