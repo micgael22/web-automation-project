4c3122531e047262426bf756fa06d3d8a9fac208
Micgael22 <micgael22@gmail.com>
2024-02-11 23:51:38 +0000

    project-Constants

---
+++ src/test/java/components/Constants.java
@@ -0,0 +1,36 @@
+package components;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import pageObjects.poLoginPage;
+import utilities.GeneralModels;
+import utilities.SeleniumHelpers;
+import java.net.MalformedURLException;
+import java.time.Duration;
+
+public class Constants extends TestListener{
+
+    public Constants() throws MalformedURLException {
+        super();
+    }
+    public WebDriver driver;
+    public poLoginPage poLog;
+    public Action uiAction;
+    public Duration interval;
+    public Duration timeout;
+    public GeneralModels general;
+    public SeleniumHelpers helpers;
+    public static String PROJECTNAME ;
+    public static String BROWSER;
+    public static String BASEURL ;
+    public static String TARGET ;
+    public FluentWait<? extends WebDriver> verificationWait;
+
+    //    public static Properties prop;
+//     SCREENSHOT / REPORT PATH
+    public static String PATH_USER_DIR = System.getProperty("user.dir");
+    public static String FILEPATHSEPARATOR = System.getProperty("file.separator");
+    public  String PATH_TO_REPORTS = "src" + FILEPATHSEPARATOR + "test"
+            + FILEPATHSEPARATOR + "resources" + FILEPATHSEPARATOR + "reports" + FILEPATHSEPARATOR;
+
+}
