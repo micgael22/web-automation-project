e67a8aa5eacb0b462f6a879512ec2d69d5612ba6
Micgael22 <micgael22@gmail.com>
2024-02-18 01:25:03 +0200

    addItems-checkoutSc

---
+++ src/test/java/scenarios/CheckoutScenario.java
@@ -0,0 +1,30 @@
+package scenarios;
+
+import components.TestListener;
+import org.openqa.selenium.WebDriver;
+import java.net.MalformedURLException;
+import java.util.logging.Logger;
+
+public class CheckoutScenario extends WebScenario{
+    private static final ThreadLocal<Logger> LOGGER = ThreadLocal.withInitial(() -> Logger.getLogger(TestListener.class.getName()));
+    public CheckoutScenario(WebDriver driver) throws MalformedURLException {
+        super(driver);
+        this.driver = driver;
+    }
+
+    public void checkoutProcess(String dataToUse) throws Exception {
+        LOGGER.get().info("Execute-Checkout-Step <<< DATA TO USE >>>" + dataToUse);
+        switch (dataToUse.toUpperCase()) {
+            case "ADD_ITEMS":
+                helpers.customVerifyElementText(poCheck.sectionProducts, "Products");
+                helpers.customClick(poCheck.btnAddBike);
+                helpers.customClick(poCheck.btnAddBPack);
+                helpers.scrollToElement(poCheck.btnAddRShirt);
+                helpers.customClick(poCheck.btnAddRShirt);
+                helpers.customClick(poCheck.iconCart);
+                helpers.customWait("3");
+                break;
+
+        }
+    }
+}
