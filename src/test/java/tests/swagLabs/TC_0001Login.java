77a5925862873c0b1a350b29ee61a04301f51bcb
Micgael22 <micgael22@gmail.com>
2024-02-17 21:12:30 +0200

    invalidPassword-login

--- src/test/java/tests/swagLabs/TC_0001Login.java
+++ src/test/java/tests/swagLabs/TC_0001Login.java
@@ -27,7 +27,7 @@ public class TC_0001Login extends BaseController {
     @Story("SWAG LABS")
     @Test(groups = {"Login"}, description = "TC_ID=02001 | Verify user able to login with valid credentials")
     public void loginValidCreds() throws Exception {
-        uiAction.executeStep("Verify Home page header present", "VERIFY ELEMENT TEXT", poLog.headerSwagLabs, "Swag Labs");
+        uiAction.executeStep("Verify Home page header present", "VERIFY_ELEMENT_TEXT", poLog.headerSwagLabs, "Swag Labs");
         uiAction.executeStep("User Enter username", "INPUT_TEXT", poLog.inputUsername, "standard_user");
         uiAction.executeStep("User Enter password", "INPUT_TEXT", poLog.inputPassword, "secret_sauce");
         uiAction.executeStep("User click on login button","CLICK",poLog.btnLogin,null);
@@ -39,16 +39,28 @@ public class TC_0001Login extends BaseController {
     @Severity(SeverityLevel.MINOR)
     @Story("Swag Labs")
     @Test(groups = {"Login"},description = "TC_ID=02002 | Verify user unable to login with invalid credentials")
-    public void loginInvalidCreds() throws Exception {
-        uiAction.executeStep("Verify Home page header present", "VERIFY ELEMENT TEXT", poLog.headerSwagLabs, "Swag Labs");
+    public void loginInvalidCreds00() throws Exception {
+        uiAction.executeStep("Verify Home page header present", "VERIFY_ELEMENT_TEXT", poLog.headerSwagLabs, "Swag Labs");
         uiAction.executeStep("User Enter username", "INPUT_TEXT", poLog.inputUsername, "inv@alidUs3rName");
         uiAction.executeStep("User Enter password", "INPUT_TEXT", poLog.inputPassword, "f@a!lp@ssw0rd");
         uiAction.executeStep("User click on login button","CLICK",poLog.btnLogin,null);
         uiAction.executeStep("Wait added","wait",null,"3");
-        uiAction.executeStep("Verify error message", "VERIFY ELEMENT TEXT", poLog.headerSwagLabs, "Epic sadface: Username and password do not match any user in this service");
+        uiAction.executeStep("Verify error message", "VERIFY_ELEMENT_TEXT", poLog.msgError, "Epic sadface: Username and password do not match any user in this service");
 
     }
 
+    @Description("Steps | Invalid Password")
+    @Severity(SeverityLevel.MINOR)
+    @Story("Swag Labs")
+    @Test(groups = {"Login"},description = "TC_ID=02003 | Verify user unable to login with invalid - password")
+    public void loginInvalidCreds01() throws Exception {
+        uiAction.executeStep("Verify Home page header present", "VERIFY_ELEMENT_TEXT", poLog.headerSwagLabs, "Swag Labs");
+        uiAction.executeStep("User Enter username", "INPUT_TEXT", poLog.inputUsername, "standard_user");
+        uiAction.executeStep("User Enter password", "INPUT_TEXT", poLog.inputPassword, "f@a!lp@ssw0rd");
+        uiAction.executeStep("User click on login button","CLICK",poLog.btnLogin,null);
+        uiAction.executeStep("Wait added","wait",null,"3");
+        uiAction.executeStep("Verify error message", "VERIFY_ELEMENT_TEXT", poLog.msgError, "Epic sadface: Username and password do not match any user in this service");
+    }
 
 
 }
