abe88f8e16104548c7d589c83815aea89caea83c
Micgael22 <micgael22@gmail.com>
2024-04-01 14:45:13 +0200

    tests-Update

--- src/test/java/tests/orangeHRM/TC_0002_Home.java
+++ src/test/java/tests/orangeHRM/TC_0002_Home.java
@@ -20,8 +20,26 @@ public class TC_0002_Home extends BaseController {
     @Test(groups = {"Dashboard"}, description = "TC_ID=06001 | Verify user can click toggle button (on and off)")
     public void dashboard_toggleButton() throws Exception {
         uiActionScenario.executeScenario("Login with user a valid details","LOGIN_VALID_HRM",null,"hrm.user00");
-        uiActionScenario.executeScenario("User navigates to enable toggle button","DASHBOARD_HRM",null,"EMP_LEAVE_TOGGLE_BTN_NAV");
+        uiActionScenario.executeScenario("User navigates to enable toggle button","DASHBOARD_HRM",null,"emp_leave_toggle_btn_nav");
         uiActionScenario.executeScenario("User reset employee leave and save","DASHBOARD_HRM",null,"reset_leave_toggle_btn_nav");
+    }
+
+    @Description("Banner Responsive")
+    @Severity(SeverityLevel.MINOR)
+    @Story("Orange HRM")
+    @Test(groups = {"Home"}, description = "TC_ID=06002 | Verify banner is responsive")
+    public void home_activeBanner() throws Exception {
+        uiActionScenario.executeScenario("Login with user a valid details","LOGIN_VALID_HRM",null,"hrm.user00");
+        uiActionScenario.executeScenario("User verify banner responsive","HOME_HRM",null,"banner_nav");
+    }
+
+    @Description("Navigates through pages")
+    @Severity(SeverityLevel.MINOR)
+    @Story("Orange HRM")
+    @Test(groups = {"Home"}, description = "TC_ID=06003 | Verify options in banner is responsive")
+    public void home_responsiveBanner() throws Exception {
+        uiActionScenario.executeScenario("Login with user a valid details","LOGIN_VALID_HRM",null,"hrm.user00");
+        uiActionScenario.executeScenario("User navigates through banner - pages","HOME_HRM",null,"banner_verify_pages");
     }
 
 
--- src/test/java/tests/swagLabs/TC_0003_Fail.java
+++ src/test/java/tests/swagLabs/TC_0003_Fail.java
@@ -16,16 +16,16 @@ public class TC_0003_Fail extends BaseController {
     @Description("Text not present")
     @Severity(SeverityLevel.MINOR)
     @Story("Swag Labs")
-    @Test(groups = {"testFAIL"}, description = "TC_ID=04001 | Verify text not present")
+    @Test(groups = {"test FAIL"}, description = "TC_ID=04001 | Verify text not present")
     public void verify_item_fail() throws Exception {
         uiActionScenario.executeScenario("Login with user a valid details","LOGIN_VALID_CREDS",null,"swaglab.user00");
-
+        uiAction.executeStep("Verify (Red T-Shirt) is present", "VERIFY_ELEMENT_TEXT", poCheck.itemTitle00, "Test.allTheThings() T-Shirt (black)");
     }
 
     @Description("locator not present")
     @Severity(SeverityLevel.MINOR)
     @Story("Swag Labs")
-    @Test(groups = {"testFAIL"}, description = "TC_ID=04002 | Unable to click on locator")
+    @Test(groups = {"test FAIL"}, description = "TC_ID=04002 | Unable to click on locator")
     public void clickable_button_fail() throws Exception {
         uiActionScenario.executeScenario("Login with user a valid details","LOGIN_VALID_CREDS",null,"swaglab.user00");
         uiActionScenario.executeScenario("User add items to cart","CHECKOUT_NAVIGATION",null,"add_items");
@@ -37,7 +37,7 @@ public class TC_0003_Fail extends BaseController {
     @Description("Incorrect URL")
     @Severity(SeverityLevel.MINOR)
     @Story("Swag Labs")
-    @Test(groups = {"testFAIL"}, description = "TC_ID=04003 | Verify incorrect URL")
+    @Test(groups = {"test FAIL"}, description = "TC_ID=04003 | Verify incorrect URL")
     public void verify_url_fail() throws Exception {
         uiActionScenario.executeScenario("Login with user a valid details","LOGIN_VALID_CREDS",null,"swaglab.user00");
         uiAction.executeStep("Verify user directed to /checkout-step-two","VERIFY_CORRECT_PAGE_URL_CONTAINS",null,"/checkout-step-two.html");
